0c71a00c2c63fc9c4062e7ec70c68e496f95edb3
JaragonCR <jorge.aragon@hpe.com>
2026-03-02 23:20:55 +0000

    feat: add versionist.conf.js to sync balena.yml and VERSION on release
    Change-type: patch

---
+++ versionist.conf.js
@@ -0,0 +1,17 @@
+module.exports = {
+  editVersion: (cwd, version, cb) => {
+    const fs = require('fs');
+    const path = require('path');
+
+    // Update balena.yml
+    const balenaYmlPath = path.join(cwd, 'balena.yml');
+    let balenaYml = fs.readFileSync(balenaYmlPath, 'utf8');
+    balenaYml = balenaYml.replace(/^version:.*$/m, `version: ${version}`);
+    fs.writeFileSync(balenaYmlPath, balenaYml);
+
+    // Update VERSION file
+    fs.writeFileSync(path.join(cwd, 'VERSION'), version + '\n');
+
+    cb();
+  }
+};
