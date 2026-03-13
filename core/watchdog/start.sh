05445ca25a5270f7d5989f7720f4f10bbb3239b2
Aleksander Vassdal <aleksander.vassdal@gmail.com>
2026-03-13 20:21:01 +0100

    chore: update device types, watchdog and compose config

--- core/watchdog/start.sh
+++ core/watchdog/start.sh
@@ -3,6 +3,27 @@
 # WiFi Watchdog startup script
 # Allows environment variable overrides with sensible defaults
 
+disable_watchdog() {
+  echo "WiFi Watchdog is disabled, exiting..."
+  exit 0
+}
+
+has_ethernet() {
+  ip link show | grep -qP '^[0-9]+: eth[^:]+:.*state UP'
+}
+
+case "${DISABLE_WIFI_WATCHDOG,,}" in
+  "1"|"true")
+    disable_watchdog
+    ;;
+  "auto")
+    if has_ethernet; then
+      echo "Ethernet connection detected, disabling WiFi Watchdog..."
+      disable_watchdog
+    fi
+    ;;
+esac
+
 export WIFI_CHECK_INTERVAL=${WIFI_CHECK_INTERVAL:-30}
 export WIFI_OFFLINE_THRESHOLD=${WIFI_OFFLINE_THRESHOLD:-600}
 export WIFI_RECOVERY_WAIT=${WIFI_RECOVERY_WAIT:-300}
